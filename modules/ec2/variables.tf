b0b7524aa87003135938355d2e08a339077eed24
devillierscbe <109145209+devillierscbe@users.noreply.github.com>
2023-04-17 16:20:59 +0530

    Create variables.tf

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "public_sg_id" {
+  description = "ID of the security group for the public subnet"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "ID of the public subnet"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair"
+  type        = string
+  default     = "demokey"
+}
